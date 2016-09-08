0dbaa3a641cb3e3cb873b99a8944618f35ce521e
Jeremy Barzas <jbarzas@gmail.com>
2016-09-08 17:45:00 -0500

    worked on custom string class

--- Intro_to_Programming/CustomStringClass/CustomStringClass.cpp
+++ Intro_to_Programming/CustomStringClass/CustomStringClass.cpp
@@ -1,15 +1,74 @@
 #include "CustomStringClass.h"
+#include "assert.h"
 
 int main()
 {
+	Student Jeremy = Student("jeremy", 1337, 25, 175, false);
+	Student Regie = Student("Regie", 9001, 21, 230, true);
+
+	
+
+	cout << "Jeremy after definiton / before swap fucntion\n\n"<< Jeremy.name << "\n" << Jeremy.ID << "\n" << Jeremy.age << "\n" << Jeremy.weight << "\n\n";
+	cout << "Regie after definiton / before swap fucntion\n\n" << Regie.name << "\n" << Regie.ID << "\n" << Regie.age << "\n" << Regie.weight << "\n\n";
+
+	Student students[2] = { Jeremy, Regie };
+
+	studentSwap(students[0], students[1]);
+
+	cout << Jeremy.name << "\n" << Jeremy.ID << "\n" << Jeremy.age << "\n" << Jeremy.weight << "\n\n";
+	cout << Regie.name << "\n" << Regie.ID << "\n" << Regie.age << "\n" << Regie.weight << "\n\n";
+
+	/*=======================================================*/
+	cout << "==============BEGIN COMPARE TEST Cstring=======\n\n";
+
 	cString testWord1 = cString("things");
-	
-	cout << testWord1.chars << "\n\n";
-
 	cString testWord2 = cString("stuff");
+	cString testWord3 = cString("stuff");
+	cString testWord4 = testWord1.append(testWord2);
+	testWord4.printWord();
 
-	cout << testWord2.chars << "\n\n";
 	
+	//cout << testWord1.chars << "\n\n";	
+	//cout << testWord2.chars << "\n\n";
+	
+	/*=======================================================*/
+	cout << "=======================================================\n\n";
+
+	cout << testWord1.sizeOf() << "\n\n";
+	cout << testWord2.sizeOf() << "\n\n";
+
+	/*=======================================================*/
+	cout << "=======================================================\n\n";
+
+	cout << testWord1.atIndex(4) << "\n\n";
+	cout << testWord2.atIndex(2) << "\n\n";
+
+	/*=======================================================*/
+	cout << "=======================================================\n\n";
+
+	int same = testWord2.compare(testWord3); //stuff.compare(stuff)
+	//-1 if before 0 if same 1 if after
+	//example: 't' is before 'u' -1
+	//t is after s so 1
+	//if you compare against two non equal length then the shorter will be -1 if all of it's characters
+	//are before up to that point
+	cout << "same is: " << same << " and it should have been: 0\n\n";
+
+	assert(testWord2.compare(testWord1) == -1, "not negative 1"); // "stuff" .compare("things") should be -1
+	
+	
+	int lexBefore = testWord1.compare(testWord2); // things.compare(stuff)
+	
+
+	/*=======================================================*/
+	cout << "=======================================================\n\n";
+
+	//cout << testWord1.append(testWord2) << "\n\n";
+	//cout << testWord2.append(testWord1) << "\n\n";
+
+
+
+
 	system("pause");
 	
 	return 0;
--- Intro_to_Programming/CustomStringClass/CustomStringClass.h
+++ Intro_to_Programming/CustomStringClass/CustomStringClass.h
@@ -3,22 +3,159 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <string>
 
+using std::string;
 using std::cout;
 using std::cin;
 
 class cString
 {
-public:
-	int size;
+private:
+	int m_size;
 	char *chars;
 
-	cString::cString();
+public:
+	
+	
 
-	cString::cString(char * c)
+	cString::cString() {};
+
+	cString::cString(char c[])
 	{
-		chars = new char[size];
+		
+		for (m_size = 0; c[m_size] != 0;)
+		{
+			m_size++;
+		}
+		
+		
+		
+		chars = new char[m_size];
+		
 		chars = c;
 	}
 
+	int cString::sizeOf()
+	{
+	
+		return m_size;
+	}
+
+	char cString::atIndex(int i)
+	{
+		char temp = chars[i];
+		
+		return temp;
+	}
+
+	int cString::compare(cString c)
+	{
+		int i = 0;
+		int result = 0;
+
+		while (true)
+		{
+			if (chars[i] == c.chars[i])
+			{
+				result = 0;
+				break;
+			}
+			
+			else if (chars[i] > c.chars[i])
+			{
+				result = 1;
+				break;
+			}
+
+			else if (chars[i] < c.chars[i])
+			{
+				result = -1;
+				break;
+			}
+
+			else
+			{
+				i++;
+			}
+		}
+
+		return result;
+	}
+
+	cString cString::append(cString other)
+	{		
+		int newSize = this->sizeOf() + other.sizeOf();
+		//need a new character array that is the size of both... + 1 if we want a null terminating character
+		char * newWord = new char[newSize];
+		//populate the newWord with the first part that is this
+		for (int i = 0; i < this->m_size; i++)
+		{
+			newWord[i] = this->chars[i];
+		}
+		//populate 
+		
+		for (int j = sizeOf(); j < other.sizeOf() + this->sizeOf(); j++)
+		{
+			int offset = j - this->sizeOf();
+			newWord[j] = other.chars[offset];
+			
+		}
+		newWord[newSize] = '\0';
+		
+		return cString(newWord);
+	
+	}
+	void printWord()
+	{
+		printf("PRINTING THE WORD!!!!!!");
+		for (int i = 0; i < m_size; i++)
+		{
+			printf("%c", chars[i]);
+		}
+		printf("\n");
+		
+	}
+
 };
+
+class Item
+{
+	Item() {}
+	
+};
+class Student
+{
+public:
+
+	string name;
+	int ID;
+	int age;
+	int weight;
+	bool alive;
+	Item * inventory;
+
+	Student::Student() {};
+
+	Student::Student(string n, int i, int a, int w, bool d)
+	{
+		name = n;
+		ID = i;
+		age = a;
+		weight = w;
+		alive = d;
+	}
+
+};
+
+void studentSwap(Student &);
+
+void studentSwap(Student &a, Student &b)
+{
+	Student temp;
+	temp = a;
+	a = b;
+	b = temp;
+
+	return;
+}
