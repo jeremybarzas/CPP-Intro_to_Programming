09512f8bf52cc344f6f793a7cbed2577cbbcd8f1
Jeremy Barzas <jbarzas@gmail.com>
2016-11-11 17:56:27 -0600

    Unit Testing start

--- Intro_to_Programming/VectorMath/Main.cpp
+++ Intro_to_Programming/VectorMath/Main.cpp
@@ -2,15 +2,7 @@
 
 int main()
 {
-	/*=================== Matrix2D =====================*/
-
-	float Mat2aVec2a[2] = { 1, 2 };
-	float Mat2aVec2b[2] = { 3, 4 };
-	Matrix2D a = Matrix2D(Mat2aVec2a, Mat2aVec2b);
-
-	float Mat2bVec2a[2] = { 5, 6 };
-	float Mat2bVec2b[2] = { 7, 8 };
-	Matrix2D b = Matrix2D(Mat2bVec2a, Mat2bVec2b);
+	UnitTest();
 
 	system("pause");
 	return 0;
--- Intro_to_Programming/VectorMath/MathLibrary.h
+++ Intro_to_Programming/VectorMath/MathLibrary.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cassert>
 
+using std::cin;
+using std::cout;
+
 /*=================== Vector Math =====================*/
 
 class Vector2D
@@ -285,7 +288,7 @@ public:
 
 class Matrix2D
 {
-private:
+
 	float m_vec2a[2];
 	float m_vec2b[2];
 
@@ -541,3 +544,67 @@ public:
 	}
 
 };
+
+/*=================== Unit Testing =====================*/
+
+void Vec2DUnitTest()
+{
+	
+}
+
+void Mat2DUnitTest()
+{
+	cout << "========== Matrix 2D Unit Test. ==========\n";
+
+	float Mat2aVec2a[2] = { 1, 2 };
+	float Mat2aVec2b[2] = { 3, 4 };
+	Matrix2D a = Matrix2D(Mat2aVec2a, Mat2aVec2b);
+
+	float Mat2bVec2a[2] = { 5, 6 };
+	float Mat2bVec2b[2] = { 7, 8 };
+	Matrix2D b = Matrix2D(Mat2bVec2a, Mat2bVec2b);
+
+	Matrix2D c = a.multiply(b);
+
+	printf("\n");
+	system("pause");
+}
+
+void UnitTest()
+{
+	int input = -1;
+
+	while (input != 0)
+	{
+		cout << "Vector 2D Unit Test - - - - - - - 1\n";
+		cout << "Vector 3D Unit Test - - - - - - - 2\n";
+		cout << "Vector 4D Unit Test - - - - - - - 3\n";
+		cout << "Matrix 2D Unit Test - - - - - - - 4\n";
+		cout << "Matrix 3D Unit Test - - - - - - - 5\n";
+		cout << "Matrix 4D Unit Test - - - - - - - 6\n";
+
+		cout << "\nEnter 0 to quit the program.\n\n";
+
+		cout << "Enter the number of the unit test to display: ";
+		cin >> input;
+		printf("\n");
+
+		switch (input)
+		{
+		case 1: 
+
+		case 2:
+
+		case 3:
+
+		case 4: Mat2DUnitTest();
+
+		case 5:
+
+		case 6:
+
+		default:
+			break;
+		}
+	}
+}
